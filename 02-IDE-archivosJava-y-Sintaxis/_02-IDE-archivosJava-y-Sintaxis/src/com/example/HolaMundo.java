eb9e85f55ac8374130a634b7395f84597c8b726e
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2022-08-09 08:26:36 +0200

    Add files via upload

---
+++ 02-IDE-archivosJava-y-Sintaxis/_02-IDE-archivosJava-y-Sintaxis/src/com/example/HolaMundo.java
@@ -0,0 +1,21 @@
+package com.example;
+
+/**
+ * Clase de prueba para crear un programa básico en Java.
+ */
+public class HolaMundo {
+    // Javadocs antes de métodos o clases
+
+    /**
+     * Punto de entrada a la aplicación Java.
+     * Permite ejecutar la aplicaicóns
+     * @param args - argumentos de entrada para la aplicación.
+     */
+    public static void main(String[] args){
+
+        System.out.println("Hola Mundo!");
+
+        // Atajo Ctrl + d para duplciar lineas
+    }
+
+}
---
+++ 02-IDE-archivosJava-y-Sintaxis/ejercicio1sesion1/src/com/example/Main.java
@@ -0,0 +1,37 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // TIPOS DE DATOS
+
+        // Números enteros
+        short variableShort = 1;
+        System.out.println(variableShort);
+        byte variableByte = 2;
+        System.out.println(variableByte);
+        int variableInt = 3;
+        System.out.println(variableInt);
+        long variableLong = 4;
+        System.out.println(variableLong);
+
+        // Numeros decimales
+        float variableFloat = 5.1f;
+        System.out.println(variableFloat);
+        double variableDouble = 5.2;
+        System.out.println(variableDouble);
+
+        // Booleanos
+        boolean variableBooleanTrue = true;
+        System.out.println(variableBooleanTrue);
+        boolean variableBooleanFalse = false;
+        System.out.println(variableBooleanFalse);
+
+        // Textos
+        char variableChar = 'a';
+        System.out.println(variableChar);
+        String variableString = "hola";
+        System.out.println(variableString);
+    }
+}
